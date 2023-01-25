242fc3f05f263c5fa3eaa21eb61bbda9f0c5b517
alexxtomas <atomasllimos@gmail.com>
2023-01-25 22:59:12 +0100

    Use custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
-import { useEffect, useState } from 'react'
-const FACTS_RANDOM = 'https://catfact.ninja/fact'
-const IMAGE_RANDOM = 'https://cataas.com/cat/says'
+import { useCatFact } from './hooks/useCatFact'
+import { useCatImage } from './hooks/useCatImage'
 
 export default function App() {
-  const [fact, setFact] = useState('')
-  const [image, setImage] = useState('')
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-  const getImage = async () => {
-    fetch(FACTS_RANDOM)
-      .then((res) => res.json())
-      .then(({ fact }) => {
-        const newWorldFact = fact.split(' ')[0]
-        setFact(fact)
-        fetch(`${IMAGE_RANDOM}/${newWorldFact}`)
-          .then((res) => res.blob())
-          .then((blob) => {
-            const imageURL = URL.createObjectURL(blob)
-            setImage(imageURL)
-            setLoading(false)
-          })
-          .catch(() => {
-            setError(true)
-          })
-      })
-  }
-  useEffect(() => {
-    getImage()
-  }, [])
+  const { catFact, refreshCatFact, loadingCatFact, errorCatFact } = useCatFact()
 
-  if (loading) return <h1>Loading</h1>
+  const { catImage, loadingCatImage, errorCatImage } = useCatImage({ fact: catFact })
 
-  if (error) return <h1>Something went wrong, please try again later</h1>
+  if (loadingCatImage || loadingCatFact) return <h1>Loading...</h1>
+
+  if (errorCatImage || errorCatFact) return <h1>Something went wrong, please try again later</h1>
 
   return (
     <div>
-      <h2>Fact: {fact}</h2>
+      <h2>Fact: {catFact}</h2>
       <h2>Image from first world of fact</h2>
-      <img src={image} />
-      <button onClick={getImage}>New Fact</button>
+      <img src={catImage} />
+      <button onClick={refreshCatFact}>New Fact</button>
     </div>
   )
 }
