197844ae06ada0953b12bdb0d432f956f22e435d
alexxtomas <atomasllimos@gmail.com>
2023-01-25 19:27:23 +0100

    Fetch data and show image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react'
+const FACTS_RANDOM = 'https://catfact.ninja/fact'
+const IMAGE_RANDOM = 'https://cataas.com/cat/says'
+
 export default function App() {
+  const [fact, setFact] = useState('')
+  const [image, setImage] = useState('')
+  useEffect(() => {
+    fetch(FACTS_RANDOM)
+      .then((res) => res.json())
+      .then(({ fact }) => {
+        const newWorldFact = fact.split(' ')[0]
+        setFact(fact)
+        fetch(`${IMAGE_RANDOM}/${newWorldFact}`)
+          .then((res) => res.blob())
+          .then((blob) => {
+            const imageURL = URL.createObjectURL(blob)
+            setImage(imageURL)
+          })
+      })
+  }, [])
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h2>Fact: {fact}</h2>
+      <h2>Image from first world of fact</h2>
+      <img src={image} />
     </div>
   )
 }
