8ecd33eb493f2b920b34075fc7a4aaef299cfabe
alexxtomas <atomasllimos@gmail.com>
2023-01-26 12:49:51 +0100

    Import styles, add classes to html elements and change html elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useCatFact } from './hooks/useCatFact'
 import { useCatImage } from './hooks/useCatImage'
+import './styles/App.css'
 
 export default function App() {
   const { catFact, refreshCatFact, loadingCatFact, errorCatFact } = useCatFact()
@@ -11,11 +12,12 @@ export default function App() {
   if (errorCatImage || errorCatFact) return <h1>Something went wrong, please try again later</h1>
 
   return (
-    <div>
-      <h2>Fact: {catFact}</h2>
-      <h2>Image from first world of fact</h2>
-      <img src={catImage} />
+    <main class='container'>
+      <section className='subcontainer'>
+        <h2>Fact: {catFact}</h2>
+        <img src={catImage} />
+      </section>
       <button onClick={refreshCatFact}>New Fact</button>
-    </div>
+    </main>
   )
 }
